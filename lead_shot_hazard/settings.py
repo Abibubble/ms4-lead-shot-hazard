c7f4e587007a771013955cb1dc009abcf899cacd
Abibubble <abi.yamaha.neo@gmail.com>
2021-08-27 22:40:52 +0000

    chore: Add mobile header, main nav bar, and footer

--- lead_shot_hazard/settings.py
+++ lead_shot_hazard/settings.py
@@ -20,7 +20,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-vkb$2m(eorsrpndz=(h+709m3xdt%0@y-h-61e9+cph7q=3+h2'
+SECRET_KEY = 'django-insecure-vkb$2m(eorsrpndz=(\
+    h+709m3xdt%0@y-h-61e9+cph7q=3+h2'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -115,16 +116,20 @@ DATABASES = {
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        'NAME': 'django.contrib.auth.\
+            password_validation.UserAttributeSimilarityValidator',
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        'NAME': 'django.contrib.auth.\
+            password_validation.MinimumLengthValidator',
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+        'NAME': 'django.contrib.auth.\
+            password_validation.CommonPasswordValidator',
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+        'NAME': 'django.contrib.auth.\
+            password_validation.NumericPasswordValidator',
     },
 ]
 
@@ -134,7 +139,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'GMT'
 
 USE_I18N = True
 
