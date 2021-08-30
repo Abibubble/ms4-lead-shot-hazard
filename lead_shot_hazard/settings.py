fa77f47564b2ec0f3bda128f22908806678a99ab
Abibubble <abi.yamaha.neo@gmail.com>
2021-08-30 16:31:55 +0000

    feat: Add bag context and delivery logic

--- lead_shot_hazard/settings.py
+++ lead_shot_hazard/settings.py
@@ -74,6 +74,7 @@ TEMPLATES = [
                 # Required by allauth
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'bag.contexts.bag_contents',
             ],
         },
     },
@@ -158,6 +159,9 @@ STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+FREE_DELIVERY_THRESHOLD = 30
+STANDARD_DELIVERY_PERCENTAGE = 10
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
