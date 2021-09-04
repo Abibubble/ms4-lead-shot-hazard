c6779b15157967aea281b583536dd42271a2d98f
Abibubble <abi.yamaha.neo@gmail.com>
2021-09-04 18:40:39 +0000

    wip: Start Stripe functionality

--- checkout/views.py
+++ checkout/views.py
@@ -14,6 +14,9 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51JKhMQDhhOiJ2FQmOr1LjXa1hwbsBh5hlHWN\
+            oWMPAvrbiuxHZt7INbhwI60jDP2Kut7w0uMoKxWg4XnEGnkLZHws00rd8VrgdN',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
