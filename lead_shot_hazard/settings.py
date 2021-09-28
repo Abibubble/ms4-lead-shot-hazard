acadca73b7fcd86558ed3621423e83ac562a3939
Abibubble <abi.yamaha.neo@gmail.com>
2021-09-28 19:21:22 +0000

    ref: Fix flake8 errors - no \ needed between brackets

--- lead_shot_hazard/settings.py
+++ lead_shot_hazard/settings.py
@@ -162,19 +162,19 @@ else:
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        'NAME': 'django.contrib.auth.password_validation.'\
+        'NAME': 'django.contrib.auth.password_validation.'
         'UserAttributeSimilarityValidator',
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.'\
+        'NAME': 'django.contrib.auth.password_validation.'
         'MinimumLengthValidator',
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.'\
+        'NAME': 'django.contrib.auth.password_validation.'
         'CommonPasswordValidator',
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.'\
+        'NAME': 'django.contrib.auth.password_validation.'
         'NumericPasswordValidator',
     },
 ]
