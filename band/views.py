057a5a65d04884444dd4306d678484093b36825c
Abibubble <abi.yamaha.neo@gmail.com>
2021-09-29 14:32:14 +0000

    feat: Add weekday and more descriptive date, edit link hovers

--- band/views.py
+++ band/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.conf import settings
 import requests
+import calendar
 
 
 def view_contact(request):
@@ -20,15 +21,33 @@ def view_gigs(request):
         band_line_up = []
         for i in range(len(bands)):
             band_line_up.append(bands[i]['displayName'])
+
+        week_days = [
+            "Monday",
+            "Tuesday",
+            "Wednesday",
+            "Thursday",
+            "Friday",
+            "Saturday",
+            "Sunday"
+        ]
+        date = event['start']['date']
+        year = int(date[:-6])
+        month = int(date[5:7])
+        day = int(date[8:])
+
         current_event = {
             'venue_name': event['venue']['displayName'],
             'band_line_up': band_line_up,
-            'date': event['start']['date'],
+            'year': year,
+            'month': calendar.month_name[month],
+            'day': day,
+            'weekday': week_days[calendar.weekday(year, month, day)],
             'time': event['start']['time'][:-3],
             'location': event['location']['city'],
             'songkick_link': event['uri']
         }
         events_dict.append(current_event)
-    print(events_dict[0]['venue_name'])
+
     template = 'band/gigs.html'
     return render(request, template, {'content': events_dict})
