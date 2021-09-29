8bb3419f7f99f3fc2cec1070b2d0543d1fc03cfd
Abibubble <abi.yamaha.neo@gmail.com>
2021-09-29 17:50:52 +0000

    feat: Add confirm deletion modal

--- products/urls.py
+++ products/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     path('add/', views.add_product, name='add_product'),
     path('edit/<int:product_id>/', views.edit_product, name='edit_product'),
     path(
-        'delete/<int:product_id>/', views.delete_product,
+        'delete/<int:product_id>/',
+        views.delete_product,
         name='delete_product'),
 ]
