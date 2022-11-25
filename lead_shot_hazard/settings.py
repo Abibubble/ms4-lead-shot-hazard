3bffb6e728d7478f127141c7ec6b16570587b96e
Abi Harrison <71400099+Abibubble@users.noreply.github.com>
2022-11-25 14:59:58 +0000

    Add Render hostname for Render deployment

--- lead_shot_hazard/settings.py
+++ lead_shot_hazard/settings.py
@@ -22,6 +22,11 @@ DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['ms4-lead-shot-hazard.herokuapp.com', 'localhost']
 
+# Add Render.com URL to allowed hosts
+RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
+if RENDER_EXTERNAL_HOSTNAME:
+    ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
+
 
 # Application definition
 
